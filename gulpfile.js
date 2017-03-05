026ec4590db6b7a7f1c7f46ad60ec4bc517a7b49
pmg1989 <972401854@qq.com>
2017-03-05 13:25:44 +0800

    add docs

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+﻿
+var gulp = require('gulp'),
+    connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+    connect.server({
+        port: 8888,
+        root: './',
+        livereload: true
+    });
+});
